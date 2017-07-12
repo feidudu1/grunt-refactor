dbff053079a9096a035263e2c830660ea23e4f1b
yangyafei <342363079@qq.com>
2017-07-12 16:20:36 +0800

    uglify插件：分别将单多店的src压缩到build 文件夹中

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,44 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         //获取package.json的信息
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        distPath: 'build',
+        // 单店
+        srcPath: 'src',
+        viewPath: 'view',
+        assetsPath: 'assets',
+
+
+        // 多店
+        msrcPath: 'm_src',
+
+        //uglify插件的配置信息
+        uglify: {
+            sim: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= srcPath %>',
+                    src: ['*.js'],
+                    dest: '<%= distPath %>',
+                    ext: '.js'
+                }]
+            },
+            mul: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= msrcPath %>',
+                    src: ['*.js'],
+                    dest: '<%= distPath %>',
+                    ext: '.js'
+                }]
+            }
+        }
+
     });
 
+    //告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     //告诉grunt当我们在终端中输入grunt时需要做些什么(注意先后顺序)
-    grunt.registerTask('default',[]);
+    grunt.registerTask('default',['uglify']);
 };
