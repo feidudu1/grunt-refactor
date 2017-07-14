41c32b1528f0284071d97a501258f95dc93e8ce0
yangyafei <342363079@qq.com>
2017-07-14 17:17:02 +0800

    replace插件：顺便修改了less到压缩css的流程，其实是被迫打乱啦

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,8 @@ module.exports = function (grunt) {
         srcPath: 'src',
         viewPath: 'view',
         assetsPath: 'assets',
+        htmlPath: 'html',
+        debugPath: 'assets/debug',
 
 
         // 多店
@@ -41,10 +43,10 @@ module.exports = function (grunt) {
         less: {
             sim: {
                 files: [{
-                    expand: true, // 设为true以下才生效？
-					cwd: '<%= assetsPath %>',
+                    expand: true, // 设为true以下才生效
+					cwd: '<%= debugPath %>/',
 					src: ['*.less','!_*.less'],
-					dest: '<%= assetsPath %>/debug',
+					dest: '<%= distPath %>',
                     ext: '.css'
                 }]
             }
@@ -52,11 +54,13 @@ module.exports = function (grunt) {
 
         cssmin: {
             sim: {
-                expand: true,
-				cwd: '<%= assetsPath %>/debug',
-				src: ['*.css'],
-				dest: '<%= distPath %>',
-                ext: '.min.css'
+                files: [{
+                    expand: true,
+    				cwd: '<%= distPath %>/',
+    				src: ['*.css'],
+    				dest: '<%= distPath %>/',
+                    ext: '.css'
+                }]
             }
         },
 
@@ -66,6 +70,29 @@ module.exports = function (grunt) {
             }
         },
 
+        replace: {
+            html: {
+                src: ['<%= viewPath %>/*.html'],
+                dest: ['<%= htmlPath %>/'],
+                replacements: [
+                    {
+                        from: /@SITENAME/g,
+                        to: '<%= cfg.global_config.site_name || "首页" %>'
+                    }
+                ]
+            },
+            less: {
+                src: ['<%= assetsPath %>/*.less'],
+                dest: ['<%= debugPath %>/'],
+                replacements: [
+                    {
+                        from: /@main-color/g,
+                        to: '<%= cfg.global_config.colors.main_color %>'
+                    }
+                ]
+            },
+        }
+
     });
 
     //告诉grunt我们将使用插件
@@ -73,10 +100,16 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
-
-
-
+    grunt.loadNpmTasks('grunt-text-replace');
 
     //告诉grunt当我们在终端中输入grunt时需要做些什么(注意先后顺序)
-    grunt.registerTask('default',['clean','uglify','less','cssmin']);
+    grunt.registerTask('sim', ['clean','uglify','replace','less','cssmin']);
+    grunt.registerTask('default','start deploy(main entry)', function () {
+        var biz = grunt.option('biz');
+        var jsonFile = biz ? biz + '_config.json' : 'config.json';  // 指定商城的配置文件（configjson文件夹）
+        var beta = biz ? 'app-' + biz : 'm.mockuai.com';   // 指定打包生成的包名，在publish文件夹下面
+        grunt.config.set('cfg', grunt.file.readJSON('configjson/' + jsonFile));
+        grunt.config.set('betaPath','publish/' + beta);
+        grunt.task.run(['sim']);
+    });
 };
